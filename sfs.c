6c2237c556814a059425b954988b263cbfde531e
rshnn-mutalisk <roshanpatel06@gmail.com>
2017-04-29 16:42:10 -0400

    missed a few includes

--- sfs.c
+++ sfs.c
@@ -22,12 +22,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #ifdef HAVE_SYS_XATTR_H
 #include <sys/xattr.h>
 #endif
 
 #include "log.h"
+#include "inode.h"
 
 
 ///////////////////////////////////////////////////////////
